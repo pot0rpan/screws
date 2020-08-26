6b01936bd56518b4230a8f9eca2ac3f88cf98688
pot <57581179+pot0rpan@users.noreply.github.com>
2020-08-26 17:45:52 -0600

    Fix weird import errors

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,3 @@
-import { addUrlProtocolIfMissing } from '../utils/urls';
-
 // Used for default/base page <title>
 export const WEBSITE_NAME = 'Screws';
 
@@ -8,13 +6,6 @@ export const URL_COLLECTION_NAME = process.env.URL_COLLECTION_NAME || 'urls';
 
 // Use full words or characters for random code generation
 export const USE_FULL_WORDS = true;
-
-// For fetch requests ran on server
-export const BASE_URL = addUrlProtocolIfMissing(
-  process.env.NEXT_PUBLIC_BASE_URL ||
-    process.env.VERCEL_URL ||
-    'http://localhost:3000'
-);
 
 export const SECRET_URLS = ['youtube.com/watch?v=dQw4w9WgXcQ'];
 
@@ -38,3 +29,16 @@ export const TRACKING_PARAMS = [/utm_\w+/, 'fbclid'];
 // Example: Once a URL is flagged for deletion a second time
 // by a different user, it gets deleted instead of flagged
 export const DELETE_FLAG_THRESHOLD = 2;
+
+// Website base url for fetch requests ran on server
+// addUrlProtocolIfMissing causes import errors if used here,
+// so it's done manually
+let baseUrl =
+  process.env.NEXT_PUBLIC_BASE_URL ||
+  process.env.VERCEL_URL ||
+  'http://localhost:3000';
+
+export const BASE_URL =
+  baseUrl.startsWith('http://') || baseUrl.startsWith('https://')
+    ? baseUrl
+    : `https://${baseUrl}`;
