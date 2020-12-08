1e86040a8d571406ea89c577dc2bab0c5f0e6aa4
pot <57581179+pot0rpan@users.noreply.github.com>
2020-12-08 15:11:14 -0700

    Fix broken tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,10 @@
 import '@testing-library/jest-dom';
 import fetchMock from 'jest-fetch-mock';
+import mockReact from 'react'; // Must begin with 'mock' or Jest error
 
 global.fetch = fetchMock;
+
+// Fix 'Warning: An update to Link inside a test was not wrapped in act(...).'
+jest.mock('next/link', () => ({ children, ...rest }) =>
+  mockReact.cloneElement(children, rest)
+);
